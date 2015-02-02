84c6ef8b106ccdf881430a37eb6858403967ce01
unknown <Aminul@CTRLSOFT-J3AEDX.(none)>
2015-02-03 01:10:08 +0600

    basic stylesheet added

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
         <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 		
-		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+		<link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
 
         <link rel="stylesheet" href="css/slicknav.css">
         <link rel="stylesheet" href="css/normalize.css">
@@ -32,29 +32,31 @@
 		<!-- header area -->
         <header>
 			<div class="container">
-				<div class="row">
-					<div class="col-md-6 col-sm-6">
-						<div class="logo fix">
-							<img src="img/logo.png" alt="" />
-							<p>Institute Canada</p>
-						</div>
-					</div>
-					<div class="col-md-6 col-sm-6">
+				<div class="row header_area">
+					<div class="col-md-10 col-sm-12 header-main-area">
 						<div class="col-md-6 col-sm-6">
-							<div class="num-email fix">
-								<p class="num">1-888-244-1106</p>
-								<p class="email">info@uni-prep.com</p>
+							<div class="logo fix">
+								<img src="img/logo.png" alt="" />
+								<p>Institute Canada</p>
 							</div>
 						</div>
 						<div class="col-md-6 col-sm-6">
-							<div class="social_icon fix">
-								<ul>
-									<li><a href=""><img src="img/fb.png" alt="" /></a></li>
-									<li><a href=""><img src="img/fb.png" alt="" /></a></li>
-									<li><a href=""><img src="img/tw.png" alt="" /></a></li>
-									<li><a href=""><img src="img/link.png" alt="" /></a></li>
-									<li><a href=""><img src="img/google.png" alt="" /></a></li>
-								</ul>
+							<div class="col-md-5 col-sm-5">
+								<div class="num-email fix">
+									<p class="num">1-888-244-1106</p>
+									<p class="email">info@uni-prep.com</p>
+								</div>
+							</div>
+							<div class="col-md-7 col-sm-7">
+								<div class="social_icon fix">
+									<ul>
+										<li><a href=""><img src="img/fb.png" alt="" /></a></li>
+										<li><a href=""><img src="img/you.png" alt="" /></a></li>
+										<li><a href=""><img src="img/tw.png" alt="" /></a></li>
+										<li><a href=""><img src="img/link.png" alt="" /></a></li>
+										<li><a href=""><img src="img/google.png" alt="" /></a></li>
+									</ul>
+								</div>
 							</div>
 						</div>
 					</div>
@@ -66,18 +68,20 @@
 		<!-- nav area -->
 		<div class="nav_area fix">
 			<div class="container">
-				<div class="row">
-					<nav class="menu">
-						<ul>
-							<li><a href=""></a>Home</li>
-							<li><a href=""></a>About us</li>
-							<li><a href=""></a>Courses</li>
-							<li><a href=""></a>Corporate training</li>
-							<li><a href=""></a>Contact</li>
-							<li><a href=""></a>Blog</li>
-							<li><a href=""></a>WP</li>
-						</ul>
-					</nav>
+				<div class="row nav_area_total">
+					<div class="col-md-10 col-sm-12 main-nav-area">
+						<nav class="menu">
+							<ul>
+								<li><a href="">Home</a></li>
+								<li><a href="">About us</a></li>
+								<li><a href="">Courses</a></li>
+								<li><a href="">Corporate training</a></li>
+								<li><a href="">Contact</a></li>
+								<li><a href="">Blog</a></li>
+								<li><a href="">WP</a></li>
+							</ul>
+						</nav>
+					</div>
 				</div>
 			</div>
 		</div>
@@ -87,7 +91,7 @@
 		<div class="bannar_area fix">
 			<div class="container">
 				<div class="row">
-					<img src="img/banar.jpg" alt="" />
+					<img class="img-responsive" src="img/banar.jpg" alt="" />
 				</div>
 			</div>
 		</div>
@@ -97,8 +101,8 @@
 		<div class="articles_area fix">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-10 col-sm-12">
-						<h2>Articles</h2>
+					<div class="col-md-10 col-sm-12 main-article-area fix">
+						<h2 class="article_header">Articles</h2>
 						<div class="col-md-6 col-sm-6">
 							<div class="single_article_area fix">
 								<div class="single_article_upper fix">
@@ -107,17 +111,17 @@
 										<p class="month">January</p>
 									</div>
 									<section class="single-article-header">
-										<h2>Teaching ESL to Children <img src="img/uni_logo.png" alt="" /></h2>
+										<h2>Teaching ESL to Children <img src="img/uni-logo.png" alt="" /></h2>
 									</section>
 								</div>
 								<div class="article-details fix">
 									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
-								</div>
-								<div class="readmore fix">
-									<a href="">Read more</a>
+									<div class="readmore fix">
+										<a href="">Read more</a>
+									</div>
 								</div>
 								<div class="article_info fix">
-									<p><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
+									<p><span class="glyphicon glyphicon-tag" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
 								</div>
 							</div>
 						</div>
@@ -129,17 +133,17 @@
 										<p class="month">January</p>
 									</div>
 									<section class="single-article-header">
-										<h2>Teaching ESL to Children <img src="img/uni_logo.png" alt="" /></h2>
+										<h2>Teaching ESL to Children <img src="img/uni-logo.png" alt="" /></h2>
 									</section>
 								</div>
 								<div class="article-details fix">
 									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
-								</div>
-								<div class="readmore fix">
-									<a href="">Read more</a>
+									<div class="readmore fix">
+										<a href="">Read more</a>
+									</div>
 								</div>
 								<div class="article_info fix">
-									<p><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
+									<p><span class="glyphicon glyphicon-tag" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
 								</div>
 							</div>
 						</div>
@@ -154,25 +158,27 @@
 		<div class="video_area fix">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-10 col-sm-12">
-						<h2>Videos</h2>
-						<div class="col-md-6 col-sm-6">
-							<div class="single_video_area fix">
-								<div class="main-video fix">
-									<iframe width="560" height="315" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
-								</div>
-								<div class="about-video fix">
-									<h2>Why Choose UNI-prep for TESOL?</h2>
+					<div class="col-md-12 col-sm-12 video_area_main fix">
+						<div class="col-md-10 col-sm-12 main-video-section">
+							<h2 class="video_header">Videos</h2>
+							<div class="col-md-6 col-sm-6">
+								<div class="single_video_area fix">
+									<div class="main-video fix">
+										<iframe width="489" height="275" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
+									</div>
+									<div class="about-video fix">
+										<h2>Why Choose UNI-prep for TESOL?</h2>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="col-md-6 col-sm-6">
-							<div class="single_video_area fix">
-								<div class="main-video fix">
-									<iframe width="560" height="315" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
-								</div>
-								<div class="about-video fix">
-									<h2>Why Choose UNI-prep for TESOL?</h2>
+							<div class="col-md-6 col-sm-6">
+								<div class="single_video_area fix">
+									<div class="main-video fix">
+										<iframe width="489" height="275" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
+									</div>
+									<div class="about-video fix">
+										<h2>Why Choose UNI-prep for TESOL?</h2>
+									</div>
 								</div>
 							</div>
 						</div>
@@ -184,13 +190,13 @@
 		
 		
 		<!-- News specilas area  -->
-		<div class="articles_area fix">
+		<div class="news_area fix">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-10 col-sm-12">
+					<div class="col-md-10 col-sm-12 news-area-main">
 						<div class="col-md-6 col-sm-6">
 							<div class="news_area fix">
-								<h2>News</h2>
+								<h2 class="news_header">News</h2>
 								<div class="single_news_heading fix">
 									<div class="date fix">
 										<p class="date">21</p>
@@ -206,7 +212,7 @@
 										<p class="month">January</p>
 									</div>
 									<section class="single-article-header">
-										<h2>Teaching ESL to Children</h2>
+										<h2>Teaching ESL to Children from Abults</h2>
 									</section>
 								</div>
 								<div class="single_news_heading fix">
@@ -214,25 +220,31 @@
 										<p class="date">21</p>
 										<p class="month">January</p>
 									</div>
-									<section class="single-article-header">
+									<section class="single-article-header fix">
 										<h2>Teaching ESL to Children</h2>
 									</section>
 								</div>
-								<a href="">Read more</a>
+								<div class="readmore fix">
+									<a href="">Read more</a>
+								</div>
 							</div>
 						</div>
 						<div class="col-md-6 col-sm-6">
 							<div class="special_area fix">
-								<h2>Specials</h2>
-								<div class="specilas_header">
-									<img src="img/apple.png" alt="" /><h2>Unlimited Courses for 1 year! </h2>
+								<h2 class="special_header">Specials</h2>
+								<div class="single_special_area fix">
+									<div class="specilas_header fix">
+										<img src="img/apple.png" alt="" /><h2 class="special-heading">Unlimited Courses <br><span>for 1 year! </span></h2>
+									</div>
+									<p style="margin-top:25px;">- TESOL Certificate and TESOL Diploma</p>
+									<p>- Business Administration Certificate and Business Administration Diploma</p>
+									<p>- Project Management Certificate and Project Management Diploma</p>
+									<p>- Marketing Management Certificate and Marketing Management Dipoma</p>
+									<p>- HR Management Certificate and HR Management Diploma</p>
+									<div class="readmore fix">
+										<a href="">Read more</a>
+									</div>
 								</div>
-								<p>-TESOL Certificate & TESOL diploma</p>
-								<p>-TESOL Certificate & TESOL diploma</p>
-								<p>-TESOL Certificate & TESOL diploma</p>
-								<p>-TESOL Certificate & TESOL diploma</p>
-								<p>-TESOL Certificate & TESOL diploma</p>
-								<a href="">Read more</a>
 							</div>
 						</div>
 					</div>
@@ -244,22 +256,22 @@
 		<!-- Student reviews area  -->
 		<div class="student_reviews_area fix">
 			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-sm-12">
-						<h2>Student reviews</h2>
+				<div class="row main_student_reviews_area fix">
+					<div class="col-md-10 col-sm-12 fix" style="margin:0 auto;float:none;">
+						<h2 class="revies-heading">Student reviews</h2>
 						<div class="col-md-6 col-sm-6">
 							<div class="single_student fix">
 								<div class="student_info fix">
 									<div class="student_photo fix">
-										<img src="img/stu.jpg" alt="" />
+										<img src="img/stu1.jpg" alt="" />
 									</div>
 									<div class="student_name fix">
-										<h1>Jhin Snow</h1>
-										<p>USA,New York</p>
+										<h1>Scarlett Johannson</h1>
+										<p>Canada, Vancouver</p>
 									</div>
 								</div>
 								<div class="student-reviews-details fix">
-									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+									<p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent vestibulum felis id elit lobortis, nec suscipit justo aliquet. Etiam maximus, erat a consequat accumsan, velit velit semper augue."</p>
 								</div>
 							</div>
 						</div>
@@ -267,7 +279,7 @@
 							<div class="single_student fix">
 								<div class="student_info fix">
 									<div class="student_photo fix">
-										<img src="img/stu.jpg" alt="" />
+										<img src="img/stu2.jpg" alt="" />
 									</div>
 									<div class="student_name fix">
 										<h1>Jhin Snow</h1>
@@ -275,7 +287,7 @@
 									</div>
 								</div>
 								<div class="student-reviews-details fix">
-									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+									<p>“Etiam vitae suscipit eros. Etiam bibendum scelerisque enim in sollicitudin. Phasellus a ultricies enim. Pellentesque iaculis ultricies vulputate. Ut ut est at mauris maximus luctus et in purus. "</p>
 								</div>
 							</div>
 						</div>
@@ -289,22 +301,22 @@
 		<div class="up_footer_area fix">
 			<div class="container">
 				<div class="row">
-					<div class="col-md-10 col-sm-12">
-						<h2>Accreditations & Affiliations</h2>
-						<div class="col-md-3 col-sm-3">
-							<img src="img/1.jpg" alt="" />
+					<div class="col-md-10 col-sm-12 fix" style="margin:0 auto;float:none;padding:10px 0px;">
+						<h2 class="up-footer-heading">Accreditations & Affiliations</h2>
+						<div class="col-md-3 col-sm-3 fix">
+							<img class="img-responsive" src="img/1.png" alt="" />
 						</div>
-						<div class="col-md-3 col-sm-3">
-							<img src="img/2.jpg" alt="" />
+						<div class="col-md-3 col-sm-3 fix">
+							<img class="img-responsive" src="img/2.png" alt="" />
 						</div>
-						<div class="col-md-2 col-sm-2">
-							<img src="img/3.jpg" alt="" />
+						<div class="col-md-2 col-sm-2 fix">
+							<img class="img-responsive" src="img/3.png" alt="" />
 						</div>
-						<div class="col-md-3 col-sm-3">
-							<img src="img/4.jpg" alt="" />
+						<div class="col-md-3 col-sm-3 fix">
+							<img class="img-responsive" src="img/4.png" alt="" />
 						</div>
-						<div class="col-md-1 col-sm-1">
-							<img src="img/5.jpg" alt="" />
+						<div class="col-md-1 col-sm-1 fix">
+							<img style="height:85px;" class="img-responsive" src="img/5.png" alt="" />
 						</div>
 					</div>
 				</div>
@@ -315,9 +327,9 @@
 		<!--  footer area  -->
 		<footer>
 			<div class="container">
-				<div class="row">
-					<div class="col-md-10 col-sm-12">
-						<div class="col-md-7 col-sm-7">
+				<div class="row" style="background:#cfcfcf;margin-top:40px; padding:15px 0px;">
+					<div class="col-md-10 col-sm-12" style="margin:0 auto;float:none;">
+						<div class="col-md-8 col-sm-8">
 							<div class="footer-menu fix">
 								<ul>
 									<li><a href=""></a>Home</li>
@@ -328,10 +340,10 @@
 									<li><a href=""></a>Blog</li>
 									<li><a href=""></a>WP</li>
 								</ul>
-								<p>&copy; 2013 UNI-Prep Corporation.All Rights Reserved</p>
 							</div>
+							<p class="copyright_txt">&copy; 2013 UNI-Prep Corporation. All Rights Reserved</p>
 						</div>
-						<div class="col-md-5 col-sm-5">
+						<div class="col-md-4 col-sm-4 footer-logo">
 							<img src="img/logo.png" alt="" />
 						</div>
 					</div>
@@ -344,6 +356,13 @@
         <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
         <script src="js/bootstrap.min.js"></script>
         <script src="js/plugins.js"></script>
+		<script src="js/jquery.fitvids.js"></script>
+		<script>
+        // Basic FitVids Test
+        $(".single_video_area").fitVids();
+        // Custom selector and No-Double-Wrapping Prevention Test
+        $(".single_video_area").fitVids({ customSelector: "iframe[src^='http://socialcam.com']"});
+      </script>
         <script src="js/main.js"></script>
     </body>
 </html>
