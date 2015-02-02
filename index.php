b93d66c5133e227f4581b000864513c17b17be12
unknown <Aminul@CTRLSOFT-J3AEDX.(none)>
2015-02-02 15:46:10 +0600

    Basic markup

---
+++ index.php
@@ -0,0 +1,349 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>This is title</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+		
+		<link href="http://netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/slicknav.css">
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="responsive.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+		
+		<!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+		
+		<!-- header area -->
+        <header>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-6">
+						<div class="logo fix">
+							<img src="img/logo.png" alt="" />
+							<p>Institute Canada</p>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-6">
+						<div class="col-md-6 col-sm-6">
+							<div class="num-email fix">
+								<p class="num">1-888-244-1106</p>
+								<p class="email">info@uni-prep.com</p>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="social_icon fix">
+								<ul>
+									<li><a href=""><img src="img/fb.png" alt="" /></a></li>
+									<li><a href=""><img src="img/fb.png" alt="" /></a></li>
+									<li><a href=""><img src="img/tw.png" alt="" /></a></li>
+									<li><a href=""><img src="img/link.png" alt="" /></a></li>
+									<li><a href=""><img src="img/google.png" alt="" /></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</header>
+		<!-- header area end -->
+		
+		<!-- nav area -->
+		<div class="nav_area fix">
+			<div class="container">
+				<div class="row">
+					<nav class="menu">
+						<ul>
+							<li><a href=""></a>Home</li>
+							<li><a href=""></a>About us</li>
+							<li><a href=""></a>Courses</li>
+							<li><a href=""></a>Corporate training</li>
+							<li><a href=""></a>Contact</li>
+							<li><a href=""></a>Blog</li>
+							<li><a href=""></a>WP</li>
+						</ul>
+					</nav>
+				</div>
+			</div>
+		</div>
+		<!-- header area end -->
+		
+		<!-- Bannar ares -->
+		<div class="bannar_area fix">
+			<div class="container">
+				<div class="row">
+					<img src="img/banar.jpg" alt="" />
+				</div>
+			</div>
+		</div>
+		<!-- Bannar area end -->
+		
+		<!-- Article area  -->
+		<div class="articles_area fix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<h2>Articles</h2>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_article_area fix">
+								<div class="single_article_upper fix">
+									<div class="date fix">
+										<p class="date">21</p>
+										<p class="month">January</p>
+									</div>
+									<section class="single-article-header">
+										<h2>Teaching ESL to Children <img src="img/uni_logo.png" alt="" /></h2>
+									</section>
+								</div>
+								<div class="article-details fix">
+									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+								</div>
+								<div class="readmore fix">
+									<a href="">Read more</a>
+								</div>
+								<div class="article_info fix">
+									<p><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_article_area fix">
+								<div class="single_article_upper fix">
+									<div class="date fix">
+										<p class="date">21</p>
+										<p class="month">January</p>
+									</div>
+									<section class="single-article-header">
+										<h2>Teaching ESL to Children <img src="img/uni_logo.png" alt="" /></h2>
+									</section>
+								</div>
+								<div class="article-details fix">
+									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+								</div>
+								<div class="readmore fix">
+									<a href="">Read more</a>
+								</div>
+								<div class="article_info fix">
+									<p><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span>Teaching Children,Teaching Tips,TESOL Certificate</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Article  area end -->
+		
+		
+		<!-- Video area  -->
+		<div class="video_area fix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<h2>Videos</h2>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_video_area fix">
+								<div class="main-video fix">
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
+								</div>
+								<div class="about-video fix">
+									<h2>Why Choose UNI-prep for TESOL?</h2>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_video_area fix">
+								<div class="main-video fix">
+									<iframe width="560" height="315" src="https://www.youtube.com/embed/XkMK07-eNw4" frameborder="0" allowfullscreen></iframe>
+								</div>
+								<div class="about-video fix">
+									<h2>Why Choose UNI-prep for TESOL?</h2>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- video area end -->
+		
+		
+		<!-- News specilas area  -->
+		<div class="articles_area fix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<div class="col-md-6 col-sm-6">
+							<div class="news_area fix">
+								<h2>News</h2>
+								<div class="single_news_heading fix">
+									<div class="date fix">
+										<p class="date">21</p>
+										<p class="month">January</p>
+									</div>
+									<section class="single-article-header">
+										<h2>Teaching ESL to Children</h2>
+									</section>
+								</div>
+								<div class="single_news_heading fix">
+									<div class="date fix">
+										<p class="date">21</p>
+										<p class="month">January</p>
+									</div>
+									<section class="single-article-header">
+										<h2>Teaching ESL to Children</h2>
+									</section>
+								</div>
+								<div class="single_news_heading fix">
+									<div class="date fix">
+										<p class="date">21</p>
+										<p class="month">January</p>
+									</div>
+									<section class="single-article-header">
+										<h2>Teaching ESL to Children</h2>
+									</section>
+								</div>
+								<a href="">Read more</a>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="special_area fix">
+								<h2>Specials</h2>
+								<div class="specilas_header">
+									<img src="img/apple.png" alt="" /><h2>Unlimited Courses for 1 year! </h2>
+								</div>
+								<p>-TESOL Certificate & TESOL diploma</p>
+								<p>-TESOL Certificate & TESOL diploma</p>
+								<p>-TESOL Certificate & TESOL diploma</p>
+								<p>-TESOL Certificate & TESOL diploma</p>
+								<p>-TESOL Certificate & TESOL diploma</p>
+								<a href="">Read more</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- News specials  area end -->
+		
+		<!-- Student reviews area  -->
+		<div class="student_reviews_area fix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<h2>Student reviews</h2>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_student fix">
+								<div class="student_info fix">
+									<div class="student_photo fix">
+										<img src="img/stu.jpg" alt="" />
+									</div>
+									<div class="student_name fix">
+										<h1>Jhin Snow</h1>
+										<p>USA,New York</p>
+									</div>
+								</div>
+								<div class="student-reviews-details fix">
+									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-6 col-sm-6">
+							<div class="single_student fix">
+								<div class="student_info fix">
+									<div class="student_photo fix">
+										<img src="img/stu.jpg" alt="" />
+									</div>
+									<div class="student_name fix">
+										<h1>Jhin Snow</h1>
+										<p>USA,New York</p>
+									</div>
+								</div>
+								<div class="student-reviews-details fix">
+									<p>SCSS and LESS have very similar syntaxes, with SASS being a slight variant on SCSS. The similarities between them allows a developer to switch seamlessly if need be. In the following demonstration I will focus on the LESS syntax but all of the concepts can be transferred to the Ruby world</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Student reviews  area end -->
+		
+		<!-- Up footer area  -->
+		<div class="up_footer_area fix">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<h2>Accreditations & Affiliations</h2>
+						<div class="col-md-3 col-sm-3">
+							<img src="img/1.jpg" alt="" />
+						</div>
+						<div class="col-md-3 col-sm-3">
+							<img src="img/2.jpg" alt="" />
+						</div>
+						<div class="col-md-2 col-sm-2">
+							<img src="img/3.jpg" alt="" />
+						</div>
+						<div class="col-md-3 col-sm-3">
+							<img src="img/4.jpg" alt="" />
+						</div>
+						<div class="col-md-1 col-sm-1">
+							<img src="img/5.jpg" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- Up footer area  end-->
+		
+		<!--  footer area  -->
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-10 col-sm-12">
+						<div class="col-md-7 col-sm-7">
+							<div class="footer-menu fix">
+								<ul>
+									<li><a href=""></a>Home</li>
+									<li><a href=""></a>About us</li>
+									<li><a href=""></a>Courses</li>
+									<li><a href=""></a>Corporate training</li>
+									<li><a href=""></a>Contact</li>
+									<li><a href=""></a>Blog</li>
+									<li><a href=""></a>WP</li>
+								</ul>
+								<p>&copy; 2013 UNI-Prep Corporation.All Rights Reserved</p>
+							</div>
+						</div>
+						<div class="col-md-5 col-sm-5">
+							<img src="img/logo.png" alt="" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</footer>
+		<!-- footer area End -->
+		
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
